9af3f565ead1528d31bba0d12a4ca93d84e40fab
Rangga Aprilio Utama <ranggaaprillio@gmail.com>
2024-09-11 19:49:32 +0700

    add admin socket ui

--- src/infrastructure/websockets/websockets.gateway.ts
+++ src/infrastructure/websockets/websockets.gateway.ts
@@ -7,38 +7,40 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from "@nestjs/websockets";
-import { Server } from "socket.io";
+import { Socket,Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
-@WebSocketGateway({
-  cors: {
-    origin: "*",
-  },
-})
+@WebSocketGateway(81, { transports: ["websocket"], cors: true })
 export class WebsocketsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   private readonly logger = new Logger(WebsocketsGateway.name);
 
   @WebSocketServer() io: Server;
+  private clients: Set<Socket> = new Set();
 
   afterInit() {
-    this.logger.log("Initialized");
+    this.logger.log("Initialized socket");
+    instrument(this.io, {
+      auth: false,
+      mode: "development",
+    });
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  handleConnection(client: any, ...args: any[]) {
+  handleConnection(client: Socket, ...args: any[]) {
     const { sockets } = this.io.sockets;
 
     this.logger.log(`Client id: ${client.id} connected`);
     this.logger.debug(`Number of connected clients: ${sockets.size}`);
   }
 
-  handleDisconnect(client: any) {
+  handleDisconnect(client: Socket) {
     this.logger.log(`Cliend id:${client.id} disconnected`);
   }
 
   @SubscribeMessage("ping")
-  handleMessage(client: any, data: any) {
+  handleMessage(client: Socket, data: any) {
     this.logger.log(`Message received from client id: ${client.id}`);
     this.logger.debug(`Payload: ${data}`);
     return {
